6ee50cde5d83f814621ba62f13c4477516f91962
Andrew Abrazovskiy <andres7480311@mail.ru>
2017-09-13 09:06:47 +0300

    Brackets/feature: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [], currOpen, result;
+	let arr = str.split('');
+	if (arr.length % 2 !== 0) return false;
+
+	result = arr.every((item) => {
+		if (findOpening(item, bracketsConfig)) {
+			stack.push(item);
+		} else {
+			currOpen = findClosing(item, bracketsConfig);
+			if (stack.pop() !== currOpen) {
+				return false;
+			} 
+		}
+		return true;
+
+	})
+
+	return !checkEqual(stack) || result === false ? false : true;
+	
+}
+
+function findClosing(bracket, config) {
+	let result;
+	config.forEach((item) => {
+		if (item[1] === bracket) result = item[0];
+	})
+
+	return result;
+}
+
+function findOpening(bracket, config) {
+	return config.some((item) => {
+		if (item[0] === bracket) return true;
+	});
+}
+
+function checkEqual(arr) {
+	return arr.length % 2 === 0 ? true : false;
 }
